e1137e871c84574d6dca87171ccd50a50f23d13d
Diksha Bhat <75386004+Diksha192001@users.noreply.github.com>
2023-08-21 00:46:23 +0530

    Prints Catalan numbers

--- catalan_number.py
+++ catalan_number.py
@@ -7,7 +7,6 @@ def catalan(n):
         for i in range(2, n):
             for j in range(0, i):
                 cat[i] += cat[j]*cat[i-j-1]
-                print("cat[i]=", cat[i] )
     return cat
 n = int(input('enter the total catalan numbers you want to be printed: '))
 print(catalan(n))
